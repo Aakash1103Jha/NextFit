4043704134f855cd2fe1e96679bc0048981cb2ba
Aakash Jha <aakash.jha1103@gmail.com>
2022-04-01 16:13:27 +0530

    Initial Commit

---
+++ NextFit/NextFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  NextFitApp.swift
+//  NextFit
+//
+//  Created by Aakash Jha on 01/04/22.
+//
+
+import SwiftUI
+
+@main
+struct NextFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
